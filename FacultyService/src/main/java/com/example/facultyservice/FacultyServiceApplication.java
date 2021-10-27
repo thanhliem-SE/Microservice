e492cd7a67bdbe0ff697ef59e8146aeee2fd9818
thanhliem-SE <thanhliem2n@gmail.com>
2021-10-27 23:23:31 +0700

    Create EurekaServer and mapping service to Server

--- FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
+++ FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.facultyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FacultyServiceApplication {
 
     public static void main(String[] args) {
