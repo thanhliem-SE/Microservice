57c1c6c0b9b2415bd36689f20ccaf56a579a285c
thanhliem-SE <thanhliem2n@gmail.com>
2021-10-31 13:18:59 +0700

    Commit retry branch

--- FacultyService/src/main/java/com/example/facultyservice/service/FacultyImpl.java
+++ FacultyService/src/main/java/com/example/facultyservice/service/FacultyImpl.java
@@ -2,18 +2,23 @@ package com.example.facultyservice.service;
 
 import com.example.facultyservice.entity.Faculty;
 import com.example.facultyservice.repository.FacultyRepository;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class FacultyImpl implements FacultyService {
     @Autowired
     private FacultyRepository facultyRepository;
 
     @Override
+    @Retry(name = "basic")
     public Faculty getFaculty(int id) {
+        log.info("IN GET FACULTY BY ID");
         return facultyRepository.findById(id).get();
     }
 
