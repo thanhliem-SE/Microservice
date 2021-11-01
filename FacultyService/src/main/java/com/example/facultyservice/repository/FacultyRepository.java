94e4a78acb20d8089e33566f705865327f9534cb
Nguyen Thanh Liem <62129407+thanhliem-SE@users.noreply.github.com>
2021-11-01 22:20:06 +0700

    fix

--- FacultyService/src/main/java/com/example/facultyservice/repository/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.facultyservice.repository;
-
-import com.example.facultyservice.entity.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
-}
