12973e7804064fc8f7b3c0822244e083212f2924
thanhliem-SE <thanhliem2n@gmail.com>
2021-11-01 22:07:13 +0700

    push jwt-branch

--- FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
+++ FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.facultyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableCaching
 public class FacultyServiceApplication {
 
     public static void main(String[] args) {
--- FacultyService/src/main/java/com/example/facultyservice/controller/FacultyRestController.java
+++ FacultyService/src/main/java/com/example/facultyservice/controller/FacultyRestController.java
@@ -2,7 +2,10 @@ package com.example.facultyservice.controller;
 
 import com.example.facultyservice.entity.Faculty;
 import com.example.facultyservice.service.FacultyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/faculties")
+@Slf4j
 public class FacultyRestController {
     @Autowired
     private FacultyService facultyService;
@@ -20,9 +24,18 @@ public class FacultyRestController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable("basic")
     public Faculty getFaculty(@PathVariable("id") int id){
+        log.info("=====GET FACULTY BY ID=====");
         return facultyService.getFaculty(id);
     }
+
+    @GetMapping("/deleteCatche")
+    @CacheEvict(value="basic", allEntries=true)
+    public String deleteCatche(){
+        log.info("=====DELETE CATCHE=====");
+        return "Delete Catche";
+    };
 
     @PostMapping("/")
     public String addFaculty(@RequestBody Faculty faculty){
